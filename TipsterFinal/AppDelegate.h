1bc8b9c13f6bc4ed7687346d359687c3457b69c5
Amrutha Srikanth <amruthasrik@fb.com>
2021-06-22 11:52:24 -0700

    Initial Commit

---
+++ TipsterFinal/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  TipsterFinal
+//
+//  Created by Amrutha Srikanth on 6/22/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ TipsterFinal/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  TipsterFinal
+//
+//  Created by Amrutha Srikanth on 6/22/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
